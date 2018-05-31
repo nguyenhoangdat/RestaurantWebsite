211451043d5b5551bf2ed99364055c36a8b5852e
Wioletta Mańka <wioletta.manka728@gmail.com>
2018-05-31 13:54:48 +0200

    Create cart using sessionStorage

--- public_html/js/foodList.js
+++ public_html/js/foodList.js
@@ -57,8 +57,11 @@ var list = [{
 ];
 
 $(function() {
+  AddFoodListToLocalStorage();
   InitializeFoodList();
+
 });
+
 
 function InitializeFoodList() {
 
@@ -66,7 +69,43 @@ function InitializeFoodList() {
   
     for (let i = 0; i < list.length; i++) {
       const element = list[i];
-      var buttonText = '<button class="btn btn-primary add" id="add' + i + '">+</button><button class="btn btn-primary" name="remove">-</button>';
+      var buttonText = '<button class="btn btn-primary add" id="' + element.name + '">+</button><button class="btn btn-primary" name="remove">-</button>';
       foodList.append("<li class='list-inline-item'><h4>" + element.name + "</h4><p>" + element.price + " zł</p><img alt=''height='200' width='200' class='img-thumbnail' src='" + element.pictureSrc + "'><br>" + buttonText + "</li>");
     }
+    
+    $( ".add" ).on( "click", function() {
+      var objFromLS = JSON.parse(localStorage.getItem(this.id));
+      sessionStorage.setItem("cart" + Math.random(), JSON.stringify(objFromLS));
+    });
   }
+
+function AddFoodListToLocalStorage(){
+  for (let i = 0; i < list.length; i++) {
+    const element = list[i];
+    localStorage.setItem(element.name, JSON.stringify(element));
+  }  
+}
+
+function GetCart(){
+  var cart = [];
+
+  Object.keys(sessionStorage).forEach(function(key){
+    console.log(sessionStorage.getItem(key));
+    var retrieveItem = JSON.parse(localStorage.getItem(key));
+    cart.push(retrieveItem);
+ });
+
+  cart.forEach(element => {
+    console.log(element);
+  });
+}
+
+function GetFoodListFromLocalStorage()
+{
+  var retrieved = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const element = localStorage[i];
+    var retrieveItem = JSON.parse(localStorage.getItem(element.name));
+    retrieved.push(retrieveItem);
+  }
+}
