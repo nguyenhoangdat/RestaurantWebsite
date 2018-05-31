240fdee27d304363c67dd2d71d3f648dc5e331f8
Wioletta Mańka <wioletta.manka728@gmail.com>
2018-05-31 16:38:38 +0200

    Improve food list managment and add Toastr

---
+++ public_html/js/list.js
@@ -0,0 +1,57 @@
+var list = [{
+    id: 1,
+    name: "Spaghetti",
+    price: 15,
+    category: "main",
+    pictureSrc: "images/food/spaghetti.jpg"
+  },
+  {
+    id: 2,
+    name: "Lazania",
+    price: 20,
+    category: "main",
+    pictureSrc: "images/food/Lasagne.jpg"
+  },
+  {
+    id: 3,
+    name: "Coca-cola",
+    price: 5,
+    category: "drinks",
+    pictureSrc: "images/food/cola.jpg"
+  },
+  {
+    id: 4,
+    name: "Piersi z kurczaka",
+    price: 23,
+    category: "main",
+    pictureSrc: "images/food/chicken.jpg"
+  },
+  {
+    id: 5,
+    name: "Zapiekany wędzony łosoś",
+    price: 30,
+    category: "main",
+    pictureSrc: "images/food/losos.jpg"
+  },
+  {
+    id: 6,
+    name: "Szarlotka z lodami",
+    price: 14,
+    category: "dessert",
+    pictureSrc: "images/food/szarlotka.jpg"
+  },
+  {
+    id: 7,
+    name: "Pierogi z mięsem",
+    price: 18,
+    category: "main",
+    pictureSrc: "images/food/pierogi.jpg"
+  },
+  {
+    id: 8,
+    name: "Omlet",
+    price: 10,
+    category: "breakfast",
+    pictureSrc: "images/food/omlet.jpg"
+  },
+  ];
---
+++ public_html/js/menuScript.js
@@ -0,0 +1,66 @@
+$(function () {
+  AddFoodListToLocalStorage();
+  InitializeFoodList();
+  UpdateTotalPrice();
+});
+
+
+function AddFoodListToLocalStorage() {
+  for (let i = 0; i < list.length; i++) {
+    const e = list[i];
+    localStorage.setItem(e.name, JSON.stringify(e));
+  }
+}
+
+function InitializeFoodList() {
+
+  var foodList = $('#foodList');
+
+  for (let i = 0; i < list.length; i++) {
+    const e = list[i];
+    var buttonText = '<button class="btn btn-primary add" id="' + e.name + '">+</button><button class="btn btn-primary remove" id="' + e.name + '">-</button>';
+    foodList.append("<li class='list-inline-item'><h4>" + e.name + "</h4><p>" + e.price + " zł</p><img alt=''height='200' width='200' class='img-thumbnail' src='" + e.pictureSrc + "'><br>" + buttonText + "</li>");
+  }
+
+  $(".add").on("click", function () {
+    var objFromLS = JSON.parse(localStorage.getItem(this.id));
+    sessionStorage.setItem("cart" + Math.random(), JSON.stringify(objFromLS));
+    UpdateTotalPrice();
+    toastr.success("Dodano " + this.id + " do koszyka");
+  });
+}
+
+function StorageToList() {
+// o.O
+  var values = [],
+    keys = Object.keys(sessionStorage),
+    i = keys.length;
+
+  while (i--) {
+    values.push(sessionStorage.getItem(keys[i]));
+  }
+
+  var r = [];
+  var listS = values;
+
+  listS.forEach(e => {
+    var itemName = e.substring(16, e.lastIndexOf(',"price') - 1);
+    list.forEach(e2 => {
+      if (e2.name == itemName) {
+        r.push(e2);
+      }
+    });
+  });
+
+  return r;
+}
+
+function UpdateTotalPrice() {
+  var sum = 0;
+  var cart = StorageToList();
+  cart.forEach(element => {
+    sum += element.price;
+  });
+  var x = document.getElementById("cart-counter");
+  x.innerHTML = "Cena: " + sum + " zł";
+}
